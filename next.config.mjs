d8fb53e364c46398882ae4230b526c8a18379cf7
Josué Castrillón <josuedcastrillonc@gmail.com>
2025-03-03 22:50:38 -0500

    Corrección de configuración para Cloud Run

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true, // Mantener si es necesario
   images: {
-    unoptimized: true, // Desactiva optimización de imágenes
+    unoptimized: true, // Cloud Run no necesita optimización de imágenes
   },
+  output: "standalone", // ⚠️ Importante: Esto permite ejecutar Next.js como servidor en Cloud Run
 };
 
 export default nextConfig;
